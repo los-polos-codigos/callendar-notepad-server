1cd75f1578bc1cba3f5da122a10a1eedc7254472
Szymon Brud <szymonbrud@stellarity.co>
2022-11-01 18:49:48 +0100

    Finish database config

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import { isReadable } from "nodemailer/lib/xoauth2";
 
 const username = encodeURIComponent("ServerRoot");
 const password = encodeURIComponent("snKjYI8yNKX4KJyH");
@@ -9,16 +8,57 @@ const authMechanism = "w=majority";
 
 const uri = `mongodb+srv://${username}:${password}@${cluster}/?${authSource}&${authMechanism}`;
 
-export const server = async () => {
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
-  client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
+// const uri = `mongodb+srv://ServerRoot:${password}@notecallendar.mqzbdke.mongodb.net/?retryWrites=true&w=majority`
 
-    client.close();
-  });
+export const server = async () => {
+
+    const client = new MongoClient(uri);
+  try{
+
+    await client.connect();
+
+
+    const result = await client.db("test").collection("test").findOne({});
+
+    console.log(result);
+    // dbo.collection("test").find({}, function(err, result) {
+    //   if (err) throw err;
+    //   console.log(result);
+    //   dbo.close();
+    // });
+
+    // const dbo = client.db('noteCallendar').collection('test.test').findOne((err, res) => {
+    //   if(err) throw err;
+    //
+    //   console.log(res.test)
+    // })
+
+    // dbo.collection("customers").findOne({}, function(err, result) {
+    //   if (err) throw err;
+    //   console.log(result.name);
+    //   db.close();
+    // });
+
+    // console.log(databasesList)
+
+    // await listDatabases(client);
+  }catch(err){
+    console.log(err);
+  }finally {
+    await client.close();
+  }
+
+
+
+  // const client = new MongoClient(uri, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   serverApi: ServerApiVersion.v1,
+  // });
+  // client.connect((err) => {
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //
+  //   client.close();
+  // });
 };
