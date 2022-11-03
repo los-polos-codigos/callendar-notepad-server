56f784445efe5e3d2268801a2c7bb7dd678fa98e
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-11-03 17:32:58 +0100

    Eslint and prettier was configured. I must add liblary to connection

--- base.test.js
+++ base.test.js
@@ -1,37 +1,36 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import request from "supertest";
-const {app} = require("./app.js");
+const { app } = require("./app.js");
 
-describe('Base tests', () => {
-    beforeAll(async () => {
-        await mongoose.connect('mongodb://localhost:27017/test-database', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-    })
-
-    afterAll(async () => {
-        await mongoose.connection.close();
+describe("Base tests", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/test-database", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+  });
 
-    it('/app-health', async () => {
-        try{
-            const response = await request(app).get('/app-health');
-            expect(response.status).toBe(200);
-            expect(response.text).toBe('OK')
-        } catch (err) {
-            throw err;
-        }
-    })
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 
-    it('/database-health', async () => {
-        try{
-            const response = await request(app).get("/database-health");
-            expect(response.status).toBe(200);
-            expect(response.text).toBe('working')
-        }catch(err){
-            throw err;
-        }
-    })
-})
+  it("/app-health", async () => {
+    try {
+      const response = await request(app).get("/app-health");
+      expect(response.status).toBe(200);
+      expect(response.text).toBe("OK");
+    } catch (err) {
+      throw err;
+    }
+  });
 
+  it("/database-health", async () => {
+    try {
+      const response = await request(app).get("/database-health");
+      expect(response.status).toBe(200);
+      expect(response.text).toBe("working");
+    } catch (err) {
+      throw err;
+    }
+  });
+});
