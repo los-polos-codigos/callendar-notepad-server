2575a52c81ec7c328bd3585268971b9b84fc13e4
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-11-04 18:40:46 +0100

    Configure scripts in package.json (new liblary to cross windows and mac platforms)

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 import { app } from "./app.js";
 
 const PORT = 2137;
-<<<<<<< HEAD
-app.get("/", (req, res) => {});
-=======
 
->>>>>>> 37a4d4363debd7fe56bbcb26dd4e35cb66e9a78f
+app.get("/", (req, res) => {});
 app.listen(PORT, () => {
   console.log(`Server listen on http://localhost:${PORT}`);
 });
