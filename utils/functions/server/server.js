4a3ecc32dd9104b3fbc5982416523bf0a83fe17e
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2023-01-06 20:31:55 +0100

    I was updated uri from server.js because username doesn't work in .env file

--- utils/functions/server/server.js
+++ utils/functions/server/server.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
+
 config();
 
-const { CLUSTER, AUTH_MECHANISM, AUTH_SOURCE } = process.env;
-const USERNAME = encodeURIComponent(process.env.DB_USER);
-const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const { CLUSTER, AUTH_MECHANISM, AUTH_SOURCE, DATABASE } = process.env;
+const PASSWORD = encodeURIComponent(process.env.PASSWORD);
 
 export let databaseErr = true;
 
 export const server = async () => {
-  const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}/?${AUTH_SOURCE}&${AUTH_MECHANISM}`;
+  const uri = `mongodb+srv://1ServerRoot1:${PASSWORD}@${CLUSTER}/${DATABASE}?${AUTH_SOURCE}&${AUTH_MECHANISM}`;
   const URL_LOCAL = "mongodb://127.0.0.1:27017/test-database";
   try {
-    await mongoose.connect(process.env.NODE_ENV === "dev" ? URL_LOCAL : uri);
+    await mongoose.connect(process.env.NODE_ENV === "dev" ? URL_LOCAL : uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB started!");
     databaseErr = false;
   } catch (err) {
