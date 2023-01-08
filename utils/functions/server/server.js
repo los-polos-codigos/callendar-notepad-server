1634637e88a7917f0e5cd149aa1e787b4e36abea
CodigoRodrigos <122213825+CodigoRodrigos@users.noreply.github.com>
2023-01-08 14:56:46 +0100

    Update server.js

--- utils/functions/server/server.js
+++ utils/functions/server/server.js
@@ -12,7 +12,7 @@ const USERNAME2 = "ServerRoot";
 export let databaseErr = true;
 
 export const server = async () => {
-  const uri = `mongodb+srv://${USERNAME2}:${PASSWORD}@${CLUSTER}/${DATABASE}?${AUTH_SOURCE}&${AUTH_MECHANISM}`;
+  const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}/${DATABASE}?${AUTH_SOURCE}&${AUTH_MECHANISM}`;
   const URL_LOCAL = "mongodb://127.0.0.1:27017/test-database";
   try {
     mongoose.set("strictQuery", false);
